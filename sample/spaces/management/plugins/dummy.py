d06c2d2f1f770a29f38a3b55e2db34c2b8029284
Saad Adel <saad.adel539@yahoo.com>
2023-03-23 11:07:06 +0200

    remove plugins folder from the sample spaces

--- sample/spaces/management/plugins/dummy.py
+++
@@ -1,9 +0,0 @@
-from models.core import PluginBase, Event
-
-
-class Plugin(PluginBase):
-    def __init__(self) -> None:
-        print('{"name": "DUMMY PLUGIN INIT"}')
-
-    async def hook(self, data: Event):
-        print('{"name": "DUMMY PLUGIN HOOK CALLED"}')
