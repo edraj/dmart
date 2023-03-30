ec3bb2ebaf8a88eadeb82427ed44a78a1b28aa63
splimterXrodina <merah.soehyb@gmail.com>
2023-03-30 18:29:25 +0100

    Fixing post_transite

--- backend/utils/ticket_sys_utils.py
+++ backend/utils/ticket_sys_utils.py
@@ -117,7 +117,7 @@ def post_transite(states, next_state: str, resolution: str):
     for state in states:
         if state["state"] == next_state:
             if "resolutions" in state:
-                available_resolutions = [one["key"] for one in state["resolutions"]]
+                available_resolutions = [one for one in state["resolutions"]]
                 if resolution in available_resolutions:
                     return {"status": True, "message": resolution}
                 else:
