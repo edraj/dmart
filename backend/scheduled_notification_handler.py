9d555e75c4c3d1c22685b5e017baae10de2c2f7b
Saad Adel <saad.adel539@yahoo.com>
2023-04-03 09:34:01 +0200

    add scheduled_notification_handler script

---
+++ backend/scheduled_notification_handler.py
@@ -0,0 +1,121 @@
+
+from datetime import datetime, timedelta
+import json
+from models.core import Content, Notification, NotificationData, Translation
+from utils.db import load as load_meta
+from utils.helpers import branch_path
+from utils.notification import NotificationManager
+from utils.redis_services import RedisServices
+from utils.repository import _save_model, _sys_update_model, get_entry_attachments
+from utils.settings import settings
+from fastapi.logger import logger
+import asyncio
+
+async def trigger_admin_notifications():
+    from_time = int((datetime.now() - timedelta(minutes=15)).timestamp()*1000)
+    to_time = int(datetime.now().timestamp()*1000)
+    async with RedisServices() as redis_services:
+        admin_notifications = await redis_services.search(
+            space_name=settings.management_space,
+            branch_name=settings.management_space_branch,
+            schema_name="admin_notification_request",
+            search=f"@subpath:/notifications/admin (-@status:finished) @scheduled_at:[{from_time} {to_time}]",
+            filters={},
+            limit=10000,
+            offset=0
+        )
+
+    if admin_notifications["total"] == 0:
+        return
+
+    notification_manager = NotificationManager()
+    for notification_doc in admin_notifications["data"]:
+        notification_dict = json.loads(notification_doc.json)
+        formatted_req = await prepare_request(notification_dict)
+
+        # Get notification receivers users
+        async with RedisServices() as redis_services:
+            receivers = await redis_services.search(
+                space_name=settings.management_space,
+                branch_name=settings.management_space_branch,
+                search=f"@subpath:users @msisdn:{'|'.join(notification_dict['msisdns'])}",
+                filters={},
+                limit=10000,
+                offset=0
+            )
+
+        if not receivers:
+            continue
+
+        # Try to send the notification 
+        # and update the notification status to finished
+        formatted_req = await prepare_request(notification_dict)
+        try:
+            for receiver in receivers['data']:
+                if not formatted_req["push_only"]:
+                    notification_obj = await Notification.from_request(notification_dict)
+                    await _save_model(
+                        "personal",
+                        f"people/{receiver}/notifications",
+                        notification_obj,
+                        notification_dict["branch_name"],
+                    )
+
+                for platform in formatted_req["platforms"]:
+                    await notification_manager.send(
+                        platform=platform,
+                        data=NotificationData(
+                            receiver=receiver,
+                            title=formatted_req["title"],
+                            body=formatted_req["body"],
+                            image_urls=formatted_req["images_urls"],
+                        ),
+                    )
+
+            notification_meta = await load_meta(
+                settings.management_space,
+                notification_dict["subpath"],
+                notification_dict["shortname"],
+                Content,
+                notification_dict["owner_shortname"],
+                settings.management_space_branch,
+            )
+            await _sys_update_model(
+                settings.management_space,
+                notification_dict["subpath"],
+                notification_meta,
+                settings.management_space_branch,
+                {"status": "finished"}
+            )
+        except Exception as e:
+            logger.error(f"Error at sending/updating admin based notification: {e.args}")
+            pass
+
+async def prepare_request(notification_dict) -> dict:
+    # Get Notification Request Images
+    attachments_path = (
+        settings.spaces_folder
+        / f"{settings.management_space}/{branch_path(notification_dict['branch_name'])}/{notification_dict['subpath']}/.dm/{notification_dict['shortname']}"
+    )
+    notification_attachments = await get_entry_attachments(
+        subpath=f"{notification_dict['subpath']}/{notification_dict['shortname']}",
+        branch_name=notification_dict["branch_name"],
+        attachments_path=attachments_path,
+    )
+    notification_images = {
+        "en": notification_attachments.get("media", {}).get("en"),
+        "ar": notification_attachments.get("media", {}).get("ar"),
+        "kd": notification_attachments.get("media", {}).get("kd"),
+    }
+
+    return {
+        "platforms": notification_dict["types"],
+        "title": Translation(**notification_dict["displayname"]),
+        "body": Translation(**notification_dict["description"]),
+        "images_urls": Translation(**notification_images),
+        "push_only": notification_dict.get("push_only", False),
+    }
+        
+
+if __name__ == "__main__":
+    asyncio.run(trigger_admin_notifications())
