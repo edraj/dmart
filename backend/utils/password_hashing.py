511c17b7d1ef2f631f47681d1282e11686e9f884
Saad Adel <saad.adel539@yahoo.com>
2023-04-19 18:35:37 +0200

    use bcrypt algorithm to hash new passwords

--- backend/utils/password_hashing.py
+++ backend/utils/password_hashing.py
@@ -1,5 +1,6 @@
 from hashlib import blake2b
 from hmac import compare_digest
+import bcrypt
 
 #! TBD to add the salt
 SECRET_KEY = b"pseudorandomly generated server secret key"
@@ -7,11 +8,35 @@ AUTH_SIZE = 32
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return compare_digest(hash_password(plain_password), hashed_password)
-    # return hash_password(plain_password) == hashed_password
+    return (
+        bcrypt_verify(plain_password, hashed_password) or 
+        hmac_verify(plain_password, hashed_password)
+    )
+
+
+def bcrypt_verify(plain_password: str, hashed_password: str) -> bool:
+    try:
+        return bcrypt.checkpw(
+            plain_password.encode('utf-8'), 
+            hashed_password.encode('utf-8')
+        )
+    except :
+        return False
+
+
+def hmac_verify(plain_password: str, hashed_password: str) -> bool:
+    try:
+        return compare_digest(hmac_hash(plain_password), hashed_password)
+    except :
+        return False
 
 
 def hash_password(password: str) -> str:
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(bytes, salt)
+
+def hmac_hash(password: str) -> str:
     h = blake2b(digest_size=AUTH_SIZE, key=SECRET_KEY)
     h.update(bytes(password, "utf-8"))
     return h.hexdigest()
