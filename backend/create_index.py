e08159925ab76d55cd1a767757683bac41d1fece
Saad Adel <saad.adel539@yahoo.com>
2023-04-26 11:46:21 +0200

    include attachements payload in entry's payload_string

--- backend/create_index.py
+++ backend/create_index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S BACKEND_ENV=config.env python3
 
 import argparse
+from copy import copy
 import json
 import re
 import traceback
@@ -10,10 +11,11 @@ import utils.db as db
 import models.core as core
 import sys
 from models.enums import ContentType, ResourceType
-from utils.helpers import branch_path, camel_case, divide_chunks, flatten_all
+from utils.helpers import branch_path, camel_case, divide_chunks
 from utils.custom_validations import validate_payload_with_schema
 from jsonschema.exceptions import ValidationError as SchemaValidationError
 from utils.redis_services import RedisServices
+from utils.repository import generate_payload_string
 from utils.settings import settings
 import utils.regex as regex
 import asyncio
@@ -106,19 +108,17 @@ async def generate_redis_docs(locators: list) -> list:
             meta_doc_id, meta_data = redis_man.prepate_meta_doc(
                 one.space_name, one.branch_name, one.subpath, meta
             )
+            payload_data = {}
             if (
                 meta.payload
                 and meta.payload.content_type == ContentType.json
                 and meta.payload.schema_shortname
             ):
-                payload_data = {}
                 try:
                     payload_path = db.payload_path(
                         one.space_name, one.subpath, myclass, one.branch_name
                     ) / str(meta.payload.body)
                     payload_data = json.loads(payload_path.read_text())
-                    payload_values_list = list(flatten_all(payload_data).values())
-                    meta_data["payload_string"] = ",".join([str(i) for i in payload_values_list])
                     await validate_payload_with_schema(
                         payload_data=payload_data,
                         space_name=one.space_name,
@@ -130,7 +130,7 @@ async def generate_redis_docs(locators: list) -> list:
                         branch_name=one.branch_name,
                         subpath=one.subpath,
                         resource_type=one.type,
-                        payload=payload_data,
+                        payload=copy(payload_data),
                         meta=meta,
                     )
                     payload.update(meta_data)
@@ -141,6 +141,14 @@ async def generate_redis_docs(locators: list) -> list:
                     )
                 except Exception as ex:
                     print(f"Error: @{one.space_name}:{one.subpath} {meta.shortname=}, {ex}")
+
+            meta_data["payload_string"] = await generate_payload_string(
+                space_name=one.space_name, 
+                subpath=one.subpath, 
+                shortname=one.shortname, 
+                branch_name=one.branch_name, 
+                payload=payload_data,
+            )
             
             redis_docs.append({"doc_id": meta_doc_id, "payload": meta_data})
 
--- backend/plugins/redis_db_update/plugin.py
+++ backend/plugins/redis_db_update/plugin.py
@@ -1,6 +1,7 @@
 import sys
-from models.core import ActionType, PluginBase, Event, Space
+from models.core import ActionType, Attachment, PluginBase, Event, Space
 from utils.helpers import camel_case
+from utils.repository import generate_payload_string
 from utils.spaces import get_spaces
 import utils.db as db
 from models import core
@@ -11,6 +12,7 @@ from fastapi.logger import logger
 
 class Plugin(PluginBase):
     async def hook(self, data: Event):
+        self.data = data
         # Type narrowing for PyRight
         if (
             not isinstance(data.shortname, str)
@@ -22,6 +24,14 @@ class Plugin(PluginBase):
 
         spaces = await get_spaces()
         if not Space.parse_raw(spaces[data.space_name]).indexing_enabled:
+            return
+
+        class_type = getattr(
+            sys.modules["models.core"],
+            camel_case(core.ResourceType(data.resource_type)),
+        )
+        if issubclass(class_type, Attachment):
+            await self.update_parent_entry_payload_string()
             return
 
         async with RedisServices() as redis_services:
@@ -54,10 +64,6 @@ class Plugin(PluginBase):
                 )
                 return
 
-            class_type = getattr(
-                sys.modules["models.core"],
-                camel_case(core.ResourceType(data.resource_type)),
-            )
             meta = await db.load(
                 space_name=data.space_name,
                 subpath=data.subpath,
@@ -113,3 +119,35 @@ class Plugin(PluginBase):
                         meta.shortname,
                         data.subpath,
                     )
+
+
+    async def update_parent_entry_payload_string(self):
+
+        async with RedisServices() as redis_services:
+            # get the parent meta doc
+            subpath_parts = self.data.subpath.strip("/").split("/")
+            if len(subpath_parts) <= 1:
+                return
+            parent_subpath, parent_shortname = "/".join(subpath_parts[:-1]), subpath_parts[-1]
+            doc_id = redis_services.generate_doc_id(
+                self.data.space_name,
+                self.data.branch_name,
+                "meta",
+                parent_shortname,
+                parent_subpath,
+            )
+            meta_doc = await redis_services.get_doc_by_id(doc_id)
+            payload_doc = await redis_services.get_doc_by_id(meta_doc.get("payload_doc_id", ""))
+            payload = {k:v for k, v in payload_doc.items() if k not in meta_doc}
+
+            # generate the payload string
+            meta_doc["payload_string"] = await generate_payload_string(
+                space_name=self.data.space_name, 
+                subpath=parent_subpath,
+                shortname=parent_shortname,
+                branch_name=self.data.branch_name, 
+                payload=payload
+            )
+
+            # update parent meta doc
+            await redis_services.save_doc(doc_id, meta_doc)
