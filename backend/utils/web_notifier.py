6f36483b378aa574a688a803a0ac04cb2888a730
Saad Adel <saad.adel539@yahoo.com>
2023-08-05 19:48:28 +0300

    cont.

--- backend/utils/web_notifier.py
+++ backend/utils/web_notifier.py
@@ -1,6 +1,5 @@
-import json
-import requests
 from models.core import NotificationData
+from utils.async_request import AsyncRequest
 from utils.notification import Notifier
 from utils.helpers import lang_code
 from utils.settings import settings
@@ -17,13 +16,12 @@ class WebNotifier(Notifier):
         if not hasattr(self, "user"):
             await self._load_user(data.receiver)
         user_lang = lang_code(self.user.language)
-        requests.post(
-            url=f"{settings.websocket_url}/send-message/{data.receiver}",
-            data=json.dumps(
-                {
+        async with AsyncRequest() as client:
+            await client.post(
+                f"{settings.websocket_url}/send-message/{data.receiver}",
+                json={
                     "title": data.title.__getattribute__(user_lang),
                     "description": data.body.__getattribute__(user_lang),
                 }
-            ),
-        )
+            )
 
