f7667ef1e4ffaa38930d62544ba8ebfe184b2eee
Saad Adel <saad.adel539@yahoo.com>
2023-08-27 14:07:57 +0300

    handle empty string data in csv exporter

--- backend/api/managed/router.py
+++ backend/api/managed/router.py
@@ -233,7 +233,7 @@ async def csv_entries(query: api.Query, user_shortname=Depends(JWTBearer())):
                     rows += list_new_rows
                             
                             
-                elif attribute_val is not None and not isinstance(attribute_val, list):
+                elif attribute_val and not isinstance(attribute_val, list):
                     new_col = attribute_val if column_key not in timestamp_fields else\
                         datetime.fromtimestamp(attribute_val).strftime('%Y-%m-%d %H:%M:%S')
                     for row in rows:
@@ -264,8 +264,8 @@ async def csv_entries(query: api.Query, user_shortname=Depends(JWTBearer())):
             attributes={"message": "The records are empty"},
         )
 
-    keys_existence = [key if keys_existence[key] else None for key in keys_existence]
-    keys = [key for key in keys if key in keys_existence]
+
+    keys = [key for key in keys if keys_existence[key]]
     v_path = StringIO()
     
     list_deprecated_keys = list(deprecated_keys)
