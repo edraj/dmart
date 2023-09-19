427768ad57ea2fd3a2f9a8c2627a4e42ad640c7c
Saad Adel <saad.adel539@yahoo.com>
2023-09-19 15:32:37 +0300

    handle empty spaces returned

--- backend/utils/repository.py
+++ backend/utils/repository.py
@@ -86,16 +86,17 @@ async def serve_query(
                     )
             if not query.sort_by:
                 query.sort_by = "ordinal"
-            record_fields = list(records[0].model_fields.keys())
-            records = sorted(
-                records,
-                key=lambda d: d.__getattribute__(query.sort_by)
-                if query.sort_by in record_fields
-                else d.attributes[query.sort_by]
-                if query.sort_by in d.attributes and d.attributes[query.sort_by] is not None
-                else 1,
-                reverse=(query.sort_type == api.SortType.descending),
-            )
+            if records:
+                record_fields = list(records[0].model_fields.keys())
+                records = sorted(
+                    records,
+                    key=lambda d: d.__getattribute__(query.sort_by)
+                    if query.sort_by in record_fields
+                    else d.attributes[query.sort_by]
+                    if query.sort_by in d.attributes and d.attributes[query.sort_by] is not None
+                    else 1,
+                    reverse=(query.sort_type == api.SortType.descending),
+                )
 
         case api.QueryType.search:
             search_res, total = await redis_query_search(query, redis_query_policies)
