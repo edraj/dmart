9e789be059f81ae0ea5edf4fa3f16f98c8582a67
Kefah Issa <kefah@imx.sh>
2023-09-26 15:09:20 +0300

    Docker is back to a working state

---
+++ admin_scripts/docker/config.ts
@@ -0,0 +1,11 @@
+export const website = {
+  title: "DMART Unified Data Platform",
+  footer: "dmart.cc unified data platform",
+  short_name: "dmart",
+  display_name: "dmart",
+  description: "dmart unified data platform",
+  languages: { ar: "العربية", en: "English" },
+  default_language: "ar",
+  backend: "http://localhost:8000/dmart/"
+  // websocket: "ws://0.0.0.0:8484/ws",
+};
