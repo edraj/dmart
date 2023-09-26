72015e610ea08bf3abc75f21d68943fe8369eeca
Kefah Issa <kefah@imx.sh>
2023-09-26 15:09:20 +0300

    Docker is back to a working state

--- admin_scripts/docker/notes.sh
+++ admin_scripts/docker/notes.sh
@@ -1,5 +1,15 @@
 
 podman build -t dmart -f Dockerfile ../../
-podman run --name dmart -p 8282:8282 -d -it dmart
+podman run --name dmart -p 8000:8000 -d -it dmart
+podman exec -it -w /home/backend  dmart /home/venv/bin/python3.11 /home/backend/create_index.py --flushall
+podman exec -it dmart /etc/init.d/dmart restart
+
+
+# List all containers
+podman ps -a --storage
+
+
+# Sample container image
+# echo -e 'FROM alpine:3.18\nRUN echo "hello world"' | podman build -t hello -
 
 
