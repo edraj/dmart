6e08e32c75e02e474d5f18fff4217aa40e2c8c7f
Kefah Issa <kefah@imx.sh>
2023-09-30 12:28:36 +0300

    Introduce set admin passwd

--- backend/password_gen.py
+++ backend/password_gen.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.11
 
 from utils.password_hashing import hash_password
 
-print("Enter the password:")
+print("Enter the password: ", end="")
 password = input()
-
+print("Generated hash: ", end="")
 print(hash_password(password))
