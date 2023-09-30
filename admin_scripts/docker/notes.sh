269c3473016f5287b7cf58570fed8f5b69afd983
Kefah Issa <kefah@imx.sh>
2023-09-30 12:37:54 +0300

    Updated notes.sh

--- admin_scripts/docker/notes.sh
+++ admin_scripts/docker/notes.sh
@@ -2,36 +2,43 @@
 
 # Steps to get a fully working instance of dmart backend + admin ui + redis
 
+# 0. Delete existing dmart container
+podman rm -f dmart
+podman rmi dmart
+
 # 1. Create the container image
-podman build -t dmart -f Dockerfile ../../
+podman build -t dmart -f Dockerfile
 
 # 2. Instaniate the the container
 podman run --name dmart -p 8000:8000 -d -it dmart
 
-# 3. Load the initial / sample space data and restart the service
-podman exec -it -w /home/backend dmart /home/venv/bin/python3.11 ./create_index.py --flushall
-podman exec -it dmart /etc/init.d/dmart restart
+# 3. Set admin and testuser password
+podman exec -it -w /home/backend dmart /home/venv/bin/python3.11 ./set_admin_passwd.py
 
-# 4. Run automated tests -- using pytest
+# 4. Load the initial / sample space data and restart the service
+podman exec -it -w /home/backend dmart bash -c 'source /home/venv/bin/activate && ./reload.sh'
+
+# 5. Run automated tests -- using pytest
 podman exec -it -w /home/backend dmart /home/venv/bin/python3.11 -m pytest
 
-# 5. Run automated tests -- using curl.sh
+# 6. Run automated tests -- using curl.sh
 podman exec -it -w /home/backend dmart ./curl.sh
 
-# 6. Print the server manifest
+# 7. Print the server manifest
 podman exec -it -w /home/backend dmart ./manifest.sh
 
-# 7. Now open the browser to http://localhost:8000
+# 8. Now open the browser to http://localhost:8000
 xdg-open http://localhost:8000
 
-# 8. Run source code linters
+# 9. Run source code linters
 podman exec -it -w /home/backend dmart bash -c 'source /home/venv/bin/activate && ./pylint.sh'
 
-# 9. Run health-check
+# 10. Run health-check
 podman exec -it -w /home/backend  dmart /home/venv/bin/python3.11 ./health_check.py
 
 # Reindex the data
-# podman exec -it -w /home/backend dmart bash -c 'source /home/venv/bin/activate && ./reload.sh'
+# podman exec -it -w /home/backend dmart /home/venv/bin/python3.11 ./create_index.py --flushall
+# podman exec -it dmart /etc/init.d/dmart restart
 
 # List all containers
 # podman ps -a --storage
