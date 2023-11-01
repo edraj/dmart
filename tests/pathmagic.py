76c31229934f87d62c8c713a05f6d8fb1911e7a1
Saad Adel <saad.adel539@yahoo.com>
2023-11-01 22:13:20 +0200

    cleaning

--- tests/pathmagic.py
+++ tests/pathmagic.py
@@ -4,9 +4,5 @@ import os
 import sys
 
 bp = os.path.dirname(os.path.realpath("."))
-# print(bp)
-# modpath = os.sep.join(bp + ["backend"])
-# # print(modpath)
 sys.path.append(f"{bp}/backend")
 sys.path.append(f"{bp}/tests")
-# print(sys.path)
