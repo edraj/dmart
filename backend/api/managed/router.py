f75e8720fc38e776549a94a99118dbfe63fc99a8
Saad Adel <saad.adel539@yahoo.com>
2023-12-11 12:51:31 +0200

    change error message

--- backend/api/managed/router.py
+++ backend/api/managed/router.py
@@ -452,7 +452,7 @@ async def serve_space(
                 api.Error(
                     type="request",
                     code=InternalErrorCode.MISSING_DATA,
-                    message="You don't have permission to this action, mismatch with the information provided",
+                    message="mismatch with the information provided",
                 ),
             )
 
@@ -1931,7 +1931,7 @@ async def get_space_report(
             error=api.Error(
                 type="access", 
                 code=InternalErrorCode.MISSING_DATA, 
-                message="You don't have permission to this action, mismatch with the information provided"
+                message="Your account is not allowed to do this action"
             ),
         )
 
