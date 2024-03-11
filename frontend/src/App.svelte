6efbfdc8cde9a3a779e4b00025d49f662f0a8115
splimter <11891837+splimter@users.noreply.github.com>
2024-03-11 14:47:38 +0000

    LightHouse improvements

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,13 +2,12 @@
   import { Router, createRouter } from "@roxi/routify";
   import routes from "../.routify/routes.default";
   import { SvelteToast } from "@zerodevx/svelte-toast";
-  import "bootstrap-icons/font/bootstrap-icons.min.css";
+  // import "bootstrap-icons/font/bootstrap-icons.min.css"
   // const rtlUrl = new URL('bootstrap/dist/css/bootstrap.rtl.min.css', import.meta.url).href;
   // const ltrUrl = new URL('bootstrap/dist/css/bootstrap.min.css', import.meta.url).href;
   // const rtlUrl = new URL('./../assets/morph/bootstrap.rtl.min.css', import.meta.url).href;
   // const ltrUrl = new URL('./../assets/morph/bootstrap.min.css', import.meta.url).href;
 
-  const router = createRouter({ routes });
   const options = {
     duration: 2500, // duration of progress bar tween to the `next` value
     initial: 1, // initial progress bar value
@@ -73,13 +72,15 @@
 </script>
 
 <svelte:head>
+  <link rel="stylesheet" media="screen" href="{new URL('bootstrap-icons/font/bootstrap-icons.min.css', import.meta.url).href}">
+  <link rel="stylesheet" media="screen" href="{new URL('./app.css', import.meta.url).href}">
   {#key $themesStore}
     {#if $dir === "rtl"}
-      <link rel="stylesheet" id="bootstrap"
+      <link rel="stylesheet" id="bootstrap" media="screen"
             href="{new URL($themesStore.rtlUrl, import.meta.url).href}"
       />
     {:else}
-      <link rel="stylesheet" id="bootstrap"
+      <link rel="stylesheet" id="bootstrap" media="screen"
             href="{new URL($themesStore.ltrUrl, import.meta.url).href}"
       />
     {/if}
@@ -92,14 +93,3 @@
     <Router {router} />
   {/await}
 </div>
-
-<style>
-  :global(.custom-toast.info) {
-    --toastBackground: rgba(72, 187, 120, 0.9);
-    --toastBarBackground: #2f855a;
-  }
-  :global(.custom-toast.warn) {
-    --toastBackground: #bb4848e6;
-    --toastBarBackground: #852f2f;
-  }
-</style>
