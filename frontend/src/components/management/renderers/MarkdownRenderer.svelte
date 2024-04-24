956c2b5a65888c06b6d750592ac0f36a0d5def72
splimter <11891837+splimter@users.noreply.github.com>
2024-04-24 09:34:50 +0100

    rm default value for MarkdownRenderer

--- frontend/src/components/management/renderers/MarkdownRenderer.svelte
+++ frontend/src/components/management/renderers/MarkdownRenderer.svelte
@@ -1,39 +1,38 @@
 <script lang="ts">
     import Tags from "@/components/management/renderers/Tags.svelte";
-
-    export let doc = "";
-    const config = {
-      tags: {
-        ListView: {
-              render: "ListView",
-              attributes: {
-                  type: {
-                      type: String,
-                      default: QueryType.subpath,
-                  },
-                  space_name: {
-                      type: String, default: "myspace"
-                  },
-                  subpath: {
-                      type: String, default: "/"
-                  },
-                  scope: {
-                      type: String, default: "public"
-                  },
-                  is_clickable: {
-                      type: Boolean, default: false
-                  },
-              }
-          }
-      }
-    };
-    const components = new Map([
-        ["ListView", ListView],
-    ]);
     import Markdoc from "@markdoc/markdoc";
     import yaml from "js-yaml";
     import ListView from "@/components/management/ListView.svelte";
     import {QueryType} from "@/dmart/index.js";
+
+    export let doc = "";
+    const config = {
+        tags: {
+            ListView: {
+                render: "ListView",
+                attributes: {
+                    type: {
+                        type: String,
+                    },
+                    space_name: {
+                        type: String,
+                    },
+                    subpath: {
+                        type: String,
+                    },
+                    scope: {
+                        type: String,
+                    },
+                    is_clickable: {
+                        type: Boolean,
+                    },
+                }
+            }
+        }
+    };
+    const components = new Map([
+        ["ListView", ListView],
+    ]);
 
     export function add_frontmatter(ast, config) {
         const frontmatter = ast.attributes.frontmatter ? yaml.load(ast.attributes.frontmatter) : {};
@@ -43,8 +42,11 @@
     }
 
     const ast = Markdoc.parse(doc);
+    console.log({ast})
     const config_with_frontmatter = add_frontmatter(ast, config);
+    console.log({config_with_frontmatter})
     const content: any = Markdoc.transform(ast, config_with_frontmatter);
+    console.log({content})
 </script>
 
 <Tags children={content.children} {components}></Tags>
