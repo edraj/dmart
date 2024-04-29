b1662468faa0fe8db281df977900a15a88261173
splimter <11891837+splimter@users.noreply.github.com>
2024-04-29 13:00:25 +0100

    impl search feat

--- frontend/src/routes/docs/_module.svelte
+++ frontend/src/routes/docs/_module.svelte
@@ -1,11 +1,14 @@
+<!-- routify:meta reset -->
 <script lang="ts">
     import {
         Row,
         Col,
         Card,
-        CardBody
+        CardBody,
     } from 'sveltestrap';
     import {url} from "@roxi/routify";
+    import Header from "@/components/Header.svelte";
+    import Footer from "@/components/Footer.svelte";
 
     const docFiles = [
         'index.md',
@@ -31,10 +34,14 @@
     @import "prismjs/themes/prism-coy.css";
 </style>
 
-<Row class="d-flex" style="height: 88vh;padding: 0px!important;" >
-  <Col sm="2" style="height:88vh;overflow-y: auto; padding: 0px!important;">
+
+<Header />
+
+<div>
+<Row class="d-flex" style="height: 90vh;padding: 0px!important;" >
+  <Col sm="2" style="border-right: solid #cecece;height:92vh;overflow-y: auto; padding: 0px!important;">
     <div class="d-flex bg-light">
-      <ul class="nav nav-pills flex-column" style="height:88vh;width: 100%;padding-top: 16px;">
+      <ul class="nav nav-pills flex-column px-2" style="height:92vh;width: 100%;padding-top: 16px;">
         {#key $url}
           {#each docFiles as file, index}
             <!-- svelte-ignore a11y-no-noninteractive-element-interactions a11y-click-events-have-key-events -->
@@ -59,7 +66,7 @@
       </style>
     </div>
   </Col>
-  <Col sm="10" style="padding: 1rem; margin-top: -1rem">
+  <Col sm="10"  style="padding-right: 1.5rem; padding-top: 1rem;">
     <Card class="px-4" style="overflow-y: auto; height:88vh">
       <CardBody>
         <slot/>
@@ -67,3 +74,8 @@
     </Card>
   </Col>
 </Row>
+</div>
+<div class="fixed-bottom">
+  <Footer />
+</div>
+
