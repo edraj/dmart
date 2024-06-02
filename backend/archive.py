381a1e6c7b2afe1b9552ca42000fe1337eaf8ef3
Saad Adel <saad.adel539@yahoo.com>
2024-06-02 12:57:18 +0300

    fix archive

--- backend/archive.py
+++ backend/archive.py
@@ -46,66 +46,67 @@ async def archive(space: str, subpath: str, schema: str, timewindow: int):
     offset = 0
     total = 10000
 
-    initial_time = time()
+    timestamp = int(time()) - (timewindow * 24 * 60 * 60)
     async with RedisServices() as redis_services:
         while offset < total:
             redis_res = await redis_services.search(
                 space_name=space,
                 branch_name=settings.default_branch,
                 schema_name=schema,
-                search="",
+                search=f"(@updated_at:[-inf {timestamp}])",
                 filters={
                     "subpath": [subpath],
                 },
                 exact_subpath=True,
                 limit=limit,
                 offset=offset,
+
             )
             if not redis_res or redis_res["total"] == 0:
                 break
+            offset += limit
+            
             search_res = redis_res["data"]
 
             for redis_document in search_res:
                 record = json.loads(redis_document)
-                # pp(record.get("created_at", ""))
-                created_at = datetime.fromtimestamp(record.get("created_at", ""))
-                if (datetime.now() - created_at).total_seconds() < 48 * 60 * 60:
-                    continue
 
-                # Move payload file
-                os.makedirs(f"{settings.spaces_folder}/archive/{space}/{subpath}", exist_ok=True)
-                shutil.move(
-                    src=f"{settings.spaces_folder}/{space}/{subpath}/{record.get('shortname')}.json",
-                    dst=f"{settings.spaces_folder}/archive/{space}/{subpath}/{record.get('shortname')}.json",
-                )
-
-                # Move meta folder / files
-                os.makedirs(
-                    f"{settings.spaces_folder}/archive/{space}/{subpath}/.dm", exist_ok=True
-                )
-                shutil.move(
-                    src=f"{settings.spaces_folder}/{space}/{subpath}/.dm/{record.get('shortname')}",
-                    dst=f"{settings.spaces_folder}/archive/{space}/{subpath}/.dm",
-                )
-
-                # Delete Payload Doc from Redis
-                await redis_services.json().delete(key=record.get("payload_doc_id"))  # type: ignore
-
-                # Delete Meta Doc from Redis
-                if "meta_doc_id" in record:
-                    await redis_services.json().delete(key=record.get("meta_doc_id"))  # type: ignore
-                else:
-                    await redis_services.delete_doc(
-                        space,
-                        settings.default_branch,
-                        "meta",
-                        record.get("shortname"),
-                        record["subpath"],
+                try:
+                    # Move payload file
+                    os.makedirs(f"{settings.spaces_folder}/archive/{space}/{subpath}", exist_ok=True)
+                    shutil.move(
+                        src=f"{settings.spaces_folder}/{space}/{subpath}/{record.get('shortname')}.json",
+                        dst=f"{settings.spaces_folder}/archive/{space}/{subpath}/{record.get('shortname')}.json",
                     )
 
-            if (time() - initial_time) > timewindow:
-                print("Time window exceeded.")
-                break
+                    # Move meta folder / files
+                    os.makedirs(
+                        f"{settings.spaces_folder}/archive/{space}/{subpath}/.dm", exist_ok=True
+                    )
+                    shutil.move(
+                        src=f"{settings.spaces_folder}/{space}/{subpath}/.dm/{record.get('shortname')}",
+                        dst=f"{settings.spaces_folder}/archive/{space}/{subpath}/.dm",
+                    )
+
+                    # Delete Payload Doc from Redis
+                    await redis_services.json().delete(key=record.get("payload_doc_id"))  # type: ignore
+
+                    # Delete Meta Doc from Redis
+                    if "meta_doc_id" in record:
+                        await redis_services.json().delete(key=record.get("meta_doc_id"))  # type: ignore
+                    else:
+                        await redis_services.delete_doc(
+                            space,
+                            settings.default_branch,
+                            "meta",
+                            record.get("shortname"),
+                            record["subpath"],
+                        )
+                except Exception as e:
+                    print(f"Error archiving {record.get('shortname')}: {e}")
+                    continue
+
+
     await RedisServices.POOL.aclose()
     await RedisServices.POOL.disconnect(True)
 
@@ -125,16 +126,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "timewindow",
         type=int,
-        help="Max execution time in seconds. Optional, default 6 hours",
-        nargs="?",
+        help="Time window in days to consider for archiving.",
     )
 
     args = parser.parse_args()
     space = args.space
     subpath = args.subpath
-    timewindow = (
-        args.timewindow * 60 * 60 if args.timewindow else 60 * 60 * 6
-    )  # 6 Hours
+    timewindow = args.timewindow
     schema = args.schema or "meta"
 
     asyncio.run(archive(space, subpath, schema, timewindow))
