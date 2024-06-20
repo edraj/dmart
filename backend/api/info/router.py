1bcb7b982e1e6eed672bd8d36ba8d967fbd274ce
splimter <11891837+splimter@users.noreply.github.com>
2024-06-20 10:59:00 +0100

    fixing version_date_cmd

--- backend/api/info/router.py
+++ backend/api/info/router.py
@@ -15,19 +15,19 @@ router = APIRouter()
 service_start_time: datetime = datetime.now()
 branch_cmd = "git rev-parse --abbrev-ref HEAD"
 result, _ = subprocess.Popen(branch_cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-branch = None if result is None or len(result) == 0 else result.strip()
+branch = None if result is None or len(result) == 0 else result.decode().strip()
 
 version_cmd = "git rev-parse --short HEAD"
 result, _ = subprocess.Popen(version_cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-version = None if result is None or len(result) == 0 else result.strip()
+version = None if result is None or len(result) == 0 else result.decode().strip()
 
 tag_cmd = "git describe --tags"
 result, _ = subprocess.Popen(tag_cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-tag = None if result is None or len(result) == 0 else result.strip()
+tag = None if result is None or len(result) == 0 else result.decode().strip()
 
 version_date_cmd = "git show --pretty=format:'%ad'"
 result, _ = subprocess.Popen(version_date_cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-version_date = None if result is None or len(result) == 0 else result.strip()
+version_date = None if result is None or len(result) == 0 else result.decode().split("\n")[0]
 
 server = socket.gethostname()
 
