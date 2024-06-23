10635695edfa88d76f16e0aebd14347894a77048
splimter <11891837+splimter@users.noreply.github.com>
2024-06-23 21:17:00 +0100

    adding dmart py

--- backend/bundler.py
+++ backend/bundler.py
@@ -2,7 +2,7 @@
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
-    'main.py',
+    'dmart.py',
     '--name=dmart',
     '--onefile',
     '--runtime-tmpdir=.',
@@ -13,4 +13,3 @@ PyInstaller.__main__.run([
     '--collect-submodules=concurrent_log_handler',
     '--clean',
 ])
-
