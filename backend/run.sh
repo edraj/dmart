d5be1a6424a13783e17f6ece5c93589f8618455e
Kefah T. Issa <kefah.issa@gmail.com>
2024-07-06 05:14:46 +0300

    Update run.sh to use ~/.venv

--- backend/run.sh
+++ backend/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$HOME/venv/bin/python3 -m hypercorn main:app --config file:utils/hypercorn_config.py
+$HOME/.venv/bin/python3 -m hypercorn main:app --config file:utils/hypercorn_config.py
