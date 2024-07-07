4989cadfb80a45246ed5f70f47f8d0e8266f2c30
Kefah T. Issa <kefah.issa@gmail.com>
2024-07-07 15:39:12 +0300

    Clean up some warning supressions

--- backend/conftest.py
+++ backend/conftest.py
@@ -2,20 +2,13 @@ from main import app
 from httpx import AsyncClient, ASGITransport
 import pytest
 
-# The following failed to work
-from typing import AsyncGenerator
-@pytest.fixture(scope="session")
-async def my_client2() -> AsyncGenerator:
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as _client:  # type: ignore
-         yield _client
-
 @pytest.fixture(scope="session")
 def anyio_backend():
     return "asyncio"
 
-@pytest.fixture()
-def my_client() -> AsyncClient:
-    return AsyncClient(transport=ASGITransport(app=app), base_url="http://test")  # type: ignore
+# @pytest.fixture()
+# def my_client() -> AsyncClient:
+#     return AsyncClient(transport=ASGITransport(app=app), base_url="http://test")  # type: ignore
 
 @pytest.fixture(scope="session")
 async def client():
