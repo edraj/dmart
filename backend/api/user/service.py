7555b4dfdbfad4b3fb5fc72fcb7060bcf8072266
splimter <11891837+splimter@users.noreply.github.com>
2024-08-07 23:23:03 +0100

    Major refactoring

--- backend/api/user/service.py
+++ backend/api/user/service.py
@@ -3,12 +3,22 @@ import random
 import string
 import urllib.parse
 import time
+from data_adapters.adapter import data_adapter as db
+from ...models import core
 from models.api import Error, Exception
+from models.enums import ContentType
+from utils import password_hashing
 from utils.async_request import AsyncRequest
 from utils.internal_error_code import InternalErrorCode
 from utils.redis_services import RedisServices
 from utils.settings import settings
+from utils.custom_validations import validate_payload_with_schema
 from fastapi.logger import logger
+from fastapi import status
+
+
+MANAGEMENT_SPACE: str = settings.management_space
+USERS_SUBPATH: str = "users"
 
 # comms_api = zain backend api
 send_otp_api = urllib.parse.urljoin(settings.comms_api, "sms/otp/send")
@@ -150,3 +160,108 @@ async def send_email(from_address: str, to_address: str, message: str, subject: 
         return False
 
     return True
+
+
+async def fetch_invitation(invitation: str):
+    if settings.active_data_db == "file":
+        async with RedisServices() as redis_services:
+            # FIXME invitation_token = await redis_services.getdel_key(
+            invitation_token = await redis_services.get_key(
+                f"users:login:invitation:{invitation}"
+            )
+    else:
+        invitation_token = await db.get_invitation_token(invitation)
+    if not invitation_token:
+        raise Exception(
+            status.HTTP_401_UNAUTHORIZED,
+            Error(
+                type="jwtauth", code=InternalErrorCode.INVALID_INVITATION, message="Invalid invitation"),
+        )
+
+    return invitation_token
+
+
+async def get_shortname_from_identifier(access_control, key, value):
+    if isinstance(value, str) and isinstance(key, str):
+        shortname = await access_control.get_user_by_criteria(key, value)
+        if not (await access_control.is_user_verified(shortname, key)):
+            raise Exception(
+                status.HTTP_401_UNAUTHORIZED,
+                Error(
+                    type="auth",
+                    code=InternalErrorCode.USER_ISNT_VERIFIED,
+                    message="This user is not verified",
+                ),
+            )
+        return shortname
+    else:
+        return None
+
+
+def check_user_validation(user, data, user_updates, invitation_token):
+    if (
+            data.get("channel") == "EMAIL"
+            and user.email
+            and f"EMAIL:{user.email}" in invitation_token
+    ):
+        user_updates["is_email_verified"] = True
+    elif (
+            data.get("channel") == "SMS"
+            and user.msisdn
+            and f"SMS:{user.msisdn}" in invitation_token
+    ):
+        user_updates["is_msisdn_verified"] = True
+
+    return user_updates
+
+
+def set_user_profile(profile, profile_user, user):
+    if profile_user.password:
+        user.password = password_hashing.hash_password(profile_user.password)
+        user.force_password_change = False
+    if "displayname" in profile.attributes:
+        user.displayname = profile_user.displayname.model_dump()
+    if "description" in profile.attributes:
+        user.description = profile_user.description.model_dump()
+    if "language" in profile.attributes:
+        user.language = profile_user.language
+
+    return user
+
+
+async def get_otp_confirmation_email_or_msisdn(profile_user):
+    async with RedisServices() as redis_services:
+        if profile_user.email:
+            return await redis_services.get_content_by_id(
+                f"users:otp:confirmation/email/{profile_user.email}"
+            )
+        elif profile_user.msisdn:
+            return await redis_services.get_content_by_id(
+                f"users:otp:confirmation/msisdn/{profile_user.msisdn}"
+            )
+
+async def update_user_payload(profile, profile_user, user, shortname):
+    separate_payload_data = {}
+    user.payload = core.Payload(
+        content_type=ContentType.json,
+        schema_shortname=profile_user.payload.schema_shortname,
+        body="",
+    )
+    if profile.attributes["payload"]["body"]:
+        separate_payload_data = profile.attributes["payload"]["body"]
+        user.payload.body = shortname + ".json"
+
+    if user.payload and separate_payload_data:
+        if profile_user.payload.schema_shortname:
+            await validate_payload_with_schema(
+                payload_data=separate_payload_data,
+                space_name=MANAGEMENT_SPACE,
+                schema_shortname=str(user.payload.schema_shortname),
+            )
+
+    await db.save_payload_from_json(
+        MANAGEMENT_SPACE,
+        USERS_SUBPATH,
+        user,
+        separate_payload_data,
+    )
