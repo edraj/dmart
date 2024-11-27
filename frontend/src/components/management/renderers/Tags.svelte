457414e1041cce2cf06e5a67204fe749cbcd0a50
splimter <11891837+splimter@users.noreply.github.com>
2024-11-27 22:04:27 +0000

    fix render issue

--- frontend/src/components/management/renderers/Tags.svelte
+++ frontend/src/components/management/renderers/Tags.svelte
@@ -1,14 +1,13 @@
 <!-- adapted from https://github.com/movingbrands/svelte-portable-text -->
 <script lang="ts">
-
     import Tags from "./Tags.svelte";
 
     let {
-      children = [],
-      components = new Map
+        children = [],
+        components = new Map
     } : {
-      children: any[],
-      components: Map<string, any>
+        children: any[],
+        components: Map<string, any>
     } = $props();
 </script>
 
@@ -16,13 +15,16 @@
   {#if typeof child === "string"}{child}{/if}
   {#if child.children}
     {#if components.has(child.name)}
-      <svelte:component this={components.get(child.name)} {...child.attributes}>
-        <Tags children={child.children} components={new Map} />
-      </svelte:component>
+      {@const Component = components.get(child.name)}
+      <Component {...child.attributes}>
+        <Tags children={child.children} components={new Map}/>
+      </Component>
     {:else}
       <svelte:element this={child.name} {...child.attributes}>
+        <Tags children={child.children} components={new Map}/>
         <Tags children={child.children} components={new Map} />
       </svelte:element>
     {/if}
   {/if}
 {/each}
+
