1f1582ae6843256da13573513d734d1a535b6101
splimter <11891837+splimter@users.noreply.github.com>
2024-12-15 17:57:55 +0000

    FIX LINT

--- backend/pylint.sh
+++ backend/pylint.sh
@@ -5,4 +5,4 @@ python -m pyright .
 echo "Ruff ..."
 python -m ruff check --exclude pytests .
 echo "Mypy ..."
-python -m mypy --explicit-package-bases --warn-return-any --check-untyped-defs --exclude loadtest --exclude pytests .
+python -m mypy --install-types --explicit-package-bases --warn-return-any --check-untyped-defs --exclude loadtest --exclude pytests .
