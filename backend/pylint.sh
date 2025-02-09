ae540eb31585d50f23662783fc0cc64e60f45ed4
Kefah T. Issa <kefah.issa@gmail.com>
2025-02-10 02:10:22 +0300

    Make pylint.sh check the return of every internal check

--- backend/pylint.sh
+++ backend/pylint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+declare -i RESULT=0
+export PYRIGHT_PYTHON_FORCE_VERSION=latest
 echo "Pyright ..."
 python -m pyright .
+RESULT+=$?
 echo "Ruff ..."
 python -m ruff check --exclude pytests --exclude alembic .
+RESULT+=$?
 echo "Mypy ..."
 python -m mypy --explicit-package-bases --warn-return-any --check-untyped-defs --exclude loadtest --exclude pytests .
+RESULT+=$?
+
+echo "Result : $RESULT"
+exit $RESULT
