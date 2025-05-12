ae62f7414b595223a8619814a1f2c28d62d9f1bd
ayamth <ayahmaitham@gmail.com>
2025-05-12 15:58:01 +0300

    new test

--- backend/pytests/feature/test_query.py
+++ backend/pytests/feature/test_query.py
@@ -340,3 +340,199 @@ async def test_save_and_query_payload_body_contents(client: AsyncClient) -> None
             assert any(
                 any("name" in project for project in r["attributes"]["payload"]["body"].get("projects", []))
                 for r in records ), "No project names found in payload"
+            
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_subpath(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={"type": QueryType.subpath, "space_name": settings.management_space, "subpath": settings.users_subpath, "limit":100},
+    )
+    json_response = response.json()
+    for record in json_response.get("records", []):
+        print(f"Shortname: {record['shortname']}, Owner: {record['attributes']['owner_shortname']}, Role: {record['attributes']['roles']}")
+
+    print(json_response)
+    
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] ==19
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_search(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "@owner_shortname:alibaba",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] == 11
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_search_negative(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "-@owner_shortname:alibaba",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] == 8
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_search_multiple(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "@owner_shortname:dmart @owner_shortname:alibaba",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] == 18
+
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_array_query(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "@roles:admin",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] == 16
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_payload_array(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "@payload.body.skills:JavaScript",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] == 3
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_payload_string(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "@payload.body.department:Art",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] == 3
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_date_single(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "@created_at:2025-05-11",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] == 16
+
+
+@pytest.mark.file_mode
+@pytest.mark.anyio
+async def test_query_date_range(client: AsyncClient) -> None:
+    response = await client.post(
+        "/managed/query",
+        json={
+            "type": QueryType.search,
+            "space_name": settings.management_space,
+            "subpath": settings.users_subpath,
+            "search": "@created_at:[2023,2025]",
+            "retrieve_json_payload": True,
+            "limit":100
+        },
+    )
+    json_response = response.json()
+    print(json_response)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert json_response["status"] == "success"
+    assert json_response["attributes"]["returned"] > 0
