a113caf2ba218714d9ba8df373d7f533e0cd9414
splimter <11891837+splimter@users.noreply.github.com>
2025-06-05 13:05:52 +0100

    fix reactivity for ticket resolutions

--- frontend/src/components/management/renderers/TicketEntryRenderer.svelte
+++ frontend/src/components/management/renderers/TicketEntryRenderer.svelte
@@ -107,6 +107,11 @@
   $effect(() => {
     ticket_action = ticketStates?.filter((e) => e.state === ticket_status)[0]?.action ?? null;
   });
+  $effect(() => {
+    if ((ticketStates??[]).length){
+      ticketResolutions = ticketPayload.states.filter((e) => e.state === ticket_status)[0]?.resolutions ?? [];
+    }
+  });
 </script>
 
 <Form class="d-flex flex-column justify-content-between p-5" on:submit={handleTicketSubmit}>
@@ -135,11 +140,11 @@
           <FormGroup>
             <Label>Resolution</Label>
             <Input
-                    class=""
-                    type="select"
-                    name="resolution"
-                    placeholder="Resolution..."
-                    bind:value={resolution}
+              class=""
+              type="select"
+              name="resolution"
+              placeholder="Resolution..."
+              bind:value={resolution}
             >
               <option value={null}>Select resolution</option>
               {#each ticketResolutions as resolution}
