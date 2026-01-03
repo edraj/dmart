4583d59fae4b5f8799dddb4c05572384b1768d4b
Soheyb Merah <11891837+splimter@users.noreply.github.com>
2026-01-03 21:58:30 +0000

    fix curl

--- backend/curl.sh
+++ backend/curl.sh
@@ -29,7 +29,7 @@ curl -s -H "Authorization: Bearer $AUTH_TOKEN" -H "$CT" $API_URL/user/profile | 
 RESULT+=$?
 
 echo -n -e "Create user from admin: \t" >&2
-curl -s -H "Authorization: Bearer $AUTH_TOKEN" -H "$CT" -d '{"space_name":"management","request_type":"create","records":[{"resource_type":"user","subpath":"users","shortname":"distributor","attributes":{"roles": ["test_role"], "msisdn": "7895412658", "email": "dummy_unqiue@gmail.com"}}]}' ${API_URL}/managed/request #| jq .status | tee /dev/stderr | grep -q "success"
+curl -s -H "Authorization: Bearer $AUTH_TOKEN" -H "$CT" -d '{"space_name":"management","request_type":"create","records":[{"resource_type":"user","subpath":"users","shortname":"distributor","attributes":{"roles": ["test_role"], "msisdn": "7895412658", "email": "dummy_unqiue@gmail.com"}}]}' ${API_URL}/managed/request | jq .status | tee /dev/stderr | grep -q "success"
 RESULT+=$?
 
 echo -n -e "update user from admin: \t" >&2
